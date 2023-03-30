997b026c4415564d71ab60027c64001f9e3b0f87
AndrushchenkoD <kennybro2016@gmaill.com>
2023-03-30 14:11:17 +0300

    master

---
+++ src/character/Character.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Character = (props) => {
+    let {name,desc,pic}=props;
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>{desc}</p>
+            <img src={pic} alt=""/>
+        </div>
+    );
+};
+
+export default Character;
